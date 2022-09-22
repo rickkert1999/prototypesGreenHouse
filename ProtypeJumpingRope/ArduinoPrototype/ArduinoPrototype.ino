f9d06485f72654119be657f994780c9938ecba15
Saartje01 <saartjevdmarkt@gmail.com>
2022-09-22 17:18:05 +0200

    Pressuresensor Changed for Button

--- ProtypeJumpingRope/ArduinoPrototype/ArduinoPrototype.ino
+++ ProtypeJumpingRope/ArduinoPrototype/ArduinoPrototype.ino
@@ -1,9 +1,9 @@
   //Set up Pins
   int pres = A3;
   int pressureValue;
+  int variable = 0;
 
   const int Led = 3; //PWM
-
 
 void setup() {
     pinMode(Led,  OUTPUT);  
@@ -13,5 +13,16 @@ void setup() {
 void loop() {
   //Set up PinMode
     pressureValue = analogRead(pres);
-    Serial.print("\tpressureValue:\t"); Serial.println(pressureValue);
+//    Serial.print("\tpressureValue:\t"); Serial.println(pressureValue);
+
+    if (pressureValue > 1)  {
+      digitalWrite(Led, HIGH);
+      variable ++;
+      Serial.println(variable);
+
+    } 
+    else {
+      digitalWrite(Led, LOW);
+    }
+
 }
